7808672de04e7b66e5c1cee41b176b94417fdfac
nelson jr mota <118323821+motanelson@users.noreply.github.com>
2025-08-01 22:07:10 +0100

    Add files via upload

--- main.c
+++ main.c
@@ -1,4 +1,19 @@
-int sum(int i,int ii){
-     return i+ii;
-
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+int eeppromX(int a,int b){ 
+  int ii=0;
+  int l=0;
+  char i[]={"hello world...\n"};
+  
+  l=strlen(i);
+  if(a>0 && a<l+1){
+	  ii=(int)i[a-1];
+  }else{if(a==0){
+			ii=(int)l;
+		}else{ii=0;}
+		}
+	
+	
+  return ii;
 }
--- test.js
+++ test.js
@@ -1,11 +1,14 @@
 console.log("\033c\033[40;33m\n")
+
 const fs = require('fs');
 const path = require('path');
+const wasmBuffer = fs.readFileSync(path.join(__dirname, 'main.wasm'));
+WebAssembly.instantiate(wasmBuffer).then(wasmModule => {
+	const { eeppromX } = wasmModule.instance.exports;
+	var d=0;
+	var e=0;
+	
+	e=eeppromX(0,0);
+	for(d=0;d<e;d++)console.log(eeppromX(d+1,0));
 
-const wasmPath = path.join(__dirname, 'main.wasm');
-const buffer = fs.readFileSync(wasmPath);
-
-WebAssembly.instantiate(buffer).then(result => {
-    const { exports } = result.instance;
-    console.log("Resultado da função:", exports.sum(5, 3));
 });
