bcfddd94cacaa0a7c36db2e22b495155847a3d88
nelson jr mota <118323821+motanelson@users.noreply.github.com>
2025-08-02 18:11:34 +0100

    Add files via upload

--- main.c
+++ main.c
@@ -1,5 +1,23 @@
 #include<stdio.h>
-int main(){
-    printf("\033c\033[43;30m\nhello world....\n");
+void start_game(){
+    printf("\033c\033[43;30m\nstart game....\n");
 
+
+}
+void main_game(){
+    printf("\033[43;30m\nmain game....\n");
+
+
+}
+void end_game(){
+    printf("\033[43;30m\nend game....\n");
+
+
+}
+
+int main(){
+    start_game();
+    main_game();
+    end_game();
+    return 0;
 }
