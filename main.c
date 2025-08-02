ee6292816fe1ae6fa49d920f085064a930ff8388
nelson jr mota <118323821+motanelson@users.noreply.github.com>
2025-08-02 13:33:14 +0100

    Add files via upload

--- main.c
+++ main.c
@@ -1,12 +1,12 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
 int sum(int a,int b){ 
   int ii=0;
   int l=0;
   char i[]={"hello world...\n"};
-  
-  l=strlen(i);
+  while(i[ii]!=0){
+       
+       ii++;
+  }
+  l=ii;
   if(a>0 && a<l+1){
 	  ii=(int)i[a-1];
   }else{if(a==0){
