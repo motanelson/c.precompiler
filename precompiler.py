a3ddc2e369cb23b5154de22244966c8fe61a4209
nelson jr mota <118323821+motanelson@users.noreply.github.com>
2025-08-06 17:44:19 +0100

    Add files via upload

---
+++ precompiler.py
@@ -0,0 +1,29 @@
+code ="""void main()
+{
+    print("");
+}
+"""
+print("\033c\033[43;30m\n\n")
+lines =0
+code2=code.split(";")
+for n in code2:
+    code3=n.split("{")
+    code33=0
+    for nn in code3:
+        code33=code33+1
+        if(len(code3)-1==code33):
+            print(str(lines)+"___________________"+"{")
+        code4=nn.split("}")
+        code44=0 
+        for nn in code4:
+            lines=lines+1
+            
+            code44=code44+1
+            if(len(code4)-1==code44):
+                print(str(lines)+"___________________"+"}")
+            code5=nn.split(")")
+            for nnn in code5:
+                
+                  code6=nnn.split("(")
+                  for nnnn in code6:
+                     print(str(lines)+"___________________"+nnnn.strip())
